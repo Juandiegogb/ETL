6ced299c382b8f4b5b68a1e447ec87ac8ca3b264
Juan Diego <juandiegar@gmail.com>
2025-02-05 15:47:16 -0500

    fx

--- src/utils/dashboards/planta_de_personal.py
+++ src/utils/dashboards/planta_de_personal.py
@@ -2,7 +2,7 @@ from time import time
 
 
 def etl(algo, warehouse):
-    with open(f"{warehouse}/{time()}.txt") as file:
+    with open(f"{warehouse}/{round(time())}.txt") as file:
         for i in range(10000):
             file.write(f"{i}\n")
 
--- src/utils/dashboards/test2.py
+++ src/utils/dashboards/test2.py
@@ -2,6 +2,6 @@ from time import time
 
 
 def etl(algo, warehouse):
-    with open(f"{warehouse}/{time()}.txt") as file:
+    with open(f"{warehouse}/{round(time())}.txt") as file:
         for i in range(10000):
             file.write(f"{i}\n")
