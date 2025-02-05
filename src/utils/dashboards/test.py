5b5ed7bfebee178d05744c983b8bfb35646e86da
Juan Diego <juandiegar@gmail.com>
2025-02-05 15:39:37 -0500

    fx

--- src/utils/dashboards/test.py
+++ src/utils/dashboards/test.py
@@ -2,6 +2,6 @@ from datetime import datetime
 
 
 def etl(algo, warehouse):
-    with open(f"{warehouse}/{datetime.now()}") as file:
+    with open(f"{warehouse}/{datetime.now()}.txt") as file:
         for i in range(10000):
             file.write(f"{i}\n")
