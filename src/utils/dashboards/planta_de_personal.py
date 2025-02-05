f0ffc3c1e7e57c34114330541bedbb347e23ff48
Juan Diego <juandiegar@gmail.com>
2025-02-05 16:05:24 -0500

    fx

--- src/utils/dashboards/planta_de_personal.py
+++ src/utils/dashboards/planta_de_personal.py
@@ -1,10 +1,11 @@
 from time import time
+from os import path
 
 
 def etl(algo, warehouse):
-    path_file = f"{round(time())}.txt"
-    with open(path_file , "a") as file:
-        for i in range(10000):
+    path_file = path.join(warehouse, f"{round(time())}.txt")
+    with open(path_file, "a") as file:
+        for i in range(1000000):
             file.write(f"{i}\n")
 
 
